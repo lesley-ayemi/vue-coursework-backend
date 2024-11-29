74468cbc5b9eb049cecacf5e3b8e4ec6857d19e6
lesley ayemi <leeayemi@gmail.com>
2024-11-29 04:54:23 +0100

    connected mongodb

--- server.js
+++ server.js
@@ -9,6 +9,25 @@ const { MongoClient } = require('mongodb');
 // Create an Express application
 const app = express();
 
+// MongoDB Atlas connection
+const MONGO_URI = 'mongodb+srv://lesley:Lesleyp1@backend-store.lprk2.mongodb.net/'; //  MongoDB Atlas URI
+const client = new MongoClient(MONGO_URI);
+
+let productsCollection;
+let ordersCollection;
+
+// Connect to MongoDB Atlas
+(async () => {
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB Atlas');
+    const database = client.db('webstore'); //  Database name
+    productsCollection = database.collection('products'); //  collection name
+    ordersCollection = database.collection('orders'); //  collection name
+  } catch (err) {
+    console.error('Error connecting to MongoDB Atlas:', err);
+  }
+})();
 
 
 
